28aa656a0f71324a0f054bb692b1af2bda6edbdc
Gabriel Henrique Souza do Nascimento <113404490+Bielhsn@users.noreply.github.com>
2025-01-31 17:17:36 -0300

    feat: otimização de imagens com next/image

--- marketplace-nft/src/components/NFTCard.tsx
+++ marketplace-nft/src/components/NFTCard.tsx
@@ -1,9 +1,10 @@
 "use client";
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { addToCart, removeFromCart } from '../redux/cartSlice'
-import { RootState } from '../redux/store'
-import styles from '../styles/NFTCard.module.css'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart, removeFromCart } from '../redux/cartSlice';
+import { RootState } from '../redux/store';
+import Image from 'next/image';
+import styles from '../styles/NFTCard.module.css';
 
 interface NFTCardProps {
   nft: {
@@ -30,13 +31,13 @@ const NFTCard: React.FC<NFTCardProps> = ({ nft }) => {
 
   return (
     <div className={styles.nftCard}>
-      <img src={nft.image} alt={nft.name} className={styles.nftImage} />
+      <Image src={nft.image} alt={nft.name} width={200} height={200} className="nft-image" />
       <h3 className={styles.title}>{nft.name}</h3>
       <p>{nft.description}</p>
       <p className={styles.price}><strong>{nft.price} ETH</strong></p>
       <button onClick={handleCartAction} className={`${styles.button} ${isInCart ? styles.added : ''}`}>
-  {isInCart ? 'Adicionado ao Carrinho' : 'Comprar'}
-</button>
+        {isInCart ? 'Adicionado ao Carrinho' : 'Comprar'}
+      </button>
 
     </div>
   )
